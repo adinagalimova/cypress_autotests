2889ba69cb3d475214f56f2391df7cf7c9f4571c
Anton Lipatov <tonybass1414@yahoo.com>
2024-04-27 19:25:02 +0500

    remove redundant shanyrak from main page

--- cypress/tests/pageObjects/mainPage.js
+++ cypress/tests/pageObjects/mainPage.js
@@ -5,23 +5,16 @@ const Button = require('../../main/elements/baseElementChildren/button');
 class MainPage extends BaseForm {
   #OGPOButton;
 
-  #shanyrakButton;
-
   #mutualButton;
 
   constructor() {
     super(new XPATH('//div[@class="ant-card-body"]'), 'main page');
     this.#OGPOButton = new Button(new XPATH('//a[@href="/ogpo/create"]'), 'OGPO button');
-    this.#shanyrakButton = new Button(new XPATH('//a[@href="/shanyrak/create"]'), 'Shanyrak button');
     this.#mutualButton = new Button(new XPATH('//a[@href="/mutual"]'), 'Mutual button');
   }
 
   clickOGPOButton() {
     this.#OGPOButton.clickElement();
-  }
-
-  clickShanyrakButton() {
-    this.#shanyrakButton.clickElement();
   }
 
   clickMutualButton() {
